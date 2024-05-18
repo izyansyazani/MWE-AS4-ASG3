d2e412e109dd3ab2704d6489a40b422bcd03524c
ivy <ivy@gmail.com>
2024-05-18 15:38:36 +0800

    parking spot page

--- EZPark/src/app/parking-spot/parking-spot.page.ts
+++ EZPark/src/app/parking-spot/parking-spot.page.ts
@@ -1,20 +1,73 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonRow,
+  IonButton,
+  IonCol,
+  IonSearchbar,
+  IonCard,
+  IonIcon,
+  IonItem,
+  IonCardContent,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardHeader,
+  IonImg,
+  IonLabel,
+  IonGrid,
+  IonButtons,
+  IonBackButton,
+} from '@ionic/angular/standalone';
 
 @Component({
-  selector: 'app-parking-spot',
-  templateUrl: './parking-spot.page.html',
-  styleUrls: ['./parking-spot.page.scss'],
+  selector: 'app-parkingspots',
+  templateUrl: './parkingspots.page.html',
+  styleUrls: ['./parkingspots.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonRow,
+    IonButton,
+    IonCol,
+    IonSearchbar,
+    IonCard,
+    IonIcon,
+    IonItem,
+    IonCardContent,
+    IonCardTitle,
+    IonCardSubtitle,
+    IonCardHeader,
+    IonImg,
+    IonLabel,
+    IonGrid,
+    IonButtons,
+    IonBackButton,
+  ],
 })
-export class ParkingSpotPage implements OnInit {
+export class ParkingspotsPage implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  goToMall() {
+    this.router.navigate(['/mall']);
   }
-
+  goToAmanHills() {
+    this.router.navigate(['/amanhill']);
+  }
+  goToAirportMall() {
+    this.router.navigate(['/airportmall']);
+  }
+  goToHome() {
+    this.router.navigate(['/home']);
+  }
 }
