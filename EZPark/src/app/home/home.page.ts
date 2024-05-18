8b4d928cf9b53e0984a7a4b7b4a0ac0bd650a3c2
ivy <ivy@gmail.com>
2024-05-18 15:37:27 +0800

    home page

--- EZPark/src/app/home/home.page.ts
+++ EZPark/src/app/home/home.page.ts
@@ -1,13 +1,92 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonRow,
+  IonButtons,
+  IonCol,
+  IonSearchbar,
+  IonCard,
+  IonIcon,
+  IonItem,
+  IonCardContent,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardHeader,
+  IonImg,
+  IonLabel,
+  IonList,
+  IonMenuButton,
+  IonButton,
+  IonMenu,
+  IonApp,
+  IonText,
+  IonGrid,
+  IonThumbnail,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonGrid,
+    IonText,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonRow,
+    IonButtons,
+    IonCol,
+    IonSearchbar,
+    IonCard,
+    IonIcon,
+    IonItem,
+    IonCardContent,
+    IonCardTitle,
+    IonCardSubtitle,
+    IonCardHeader,
+    IonImg,
+    IonLabel,
+    IonList,
+    IonMenuButton,
+    IonButton,
+    IonMenu,
+    IonApp,
+    IonThumbnail,
+    RouterModule,
+  ],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  arthurText = 'Arthur B.';
+  findParkingText = 'Find Parking';
+  recentParkingTitle = 'Recent Parking';
+  favouriteParkingTitle = 'Favourite Parking';
+
+  parkingLocations = [
+    {
+      title: 'The Mall, Gadong',
+      imageUrl: '../assets/TheMall.jpg',
+    },
+  ];
+
+  favouriteParking = [
+    {
+      title: 'The Mall, Gadong',
+      imageUrl: '../assets/TheMall.jpg',
+    },
+    {
+      title: 'Shopping Mall Badong',
+      imageUrl: '../assets/ShoppingMall.jpg',
+    },
+  ];
+
   constructor() {}
+
+  ngOnInit() {}
 }
