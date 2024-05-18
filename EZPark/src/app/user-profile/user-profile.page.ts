400ee59a4f71a996a4e559062cf14bc24a4bfe41
ivy <ivy@gmail.com>
2024-05-18 15:58:03 +0800

    user profile page

--- EZPark/src/app/user-profile/user-profile.page.ts
+++ EZPark/src/app/user-profile/user-profile.page.ts
@@ -1,20 +1,123 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { RouterModule, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonRow,
+  IonButtons,
+  IonCol,
+  IonSearchbar,
+  IonCard,
+  IonIcon,
+  IonItem,
+  IonCardContent,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardHeader,
+  IonImg,
+  IonLabel,
+  IonList,
+  IonRouterLink,
+  IonFooter,
+  IonAvatar,
+  IonBackButton,
+} from '@ionic/angular/standalone';
 
 @Component({
-  selector: 'app-user-profile',
-  templateUrl: './user-profile.page.html',
-  styleUrls: ['./user-profile.page.scss'],
+  selector: 'app-userprofile',
+  templateUrl: './userprofile.page.html',
+  styleUrls: ['./userprofile.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonCol,
+    IonButtons,
+    IonRow,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonSearchbar,
+    IonCard,
+    IonIcon,
+    IonItem,
+    IonCardContent,
+    IonCardTitle,
+    IonCardSubtitle,
+    IonCardHeader,
+    IonImg,
+    IonLabel,
+    IonList,
+    IonRouterLink,
+    IonFooter,
+    IonAvatar,
+    IonBackButton,
+    RouterModule,
+  ],
 })
-export class UserProfilePage implements OnInit {
+export class UserprofilePage implements OnInit {
+  profile = {
+    name: 'Arthur B',
+    email: 'arthur.b@gmail.com',
+    phone: '+973 800 8000',
+    image: 'assets/profile-pic.jpg',
+    payment: '**** **** **** 1234',
+    notification: 'Enabled',
+    security: 'High',
+  };
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private alertController: AlertController
+  ) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+  goToHome() {
+    this.router.navigate(['/home']);
   }
 
+  editProfile() {
+    this.router.navigate(['/userprofile']);
+  }
+
+  parkingHistory() {
+    this.router.navigate(['/history']);
+  }
+
+  aboutUs() {
+    this.router.navigate(['/aboutus']);
+  }
+
+  feedback() {
+    this.router.navigate(['/feedback']);
+  }
+
+  logout() {
+    this.alertController
+      .create({
+        header: 'Confirm Logout',
+        message: 'Are you sure you want to logout?',
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            handler: () => {},
+          },
+          {
+            text: 'Logout',
+            handler: () => {
+              this.router.navigate(['/signup-login']);
+            },
+          },
+        ],
+      })
+      .then((alert) => alert.present());
+  }
 }
