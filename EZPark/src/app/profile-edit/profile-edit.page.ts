79b576ae79ad921738f9704b6cc1399a09b23fd8
ivy <ivy@gmail.com>
2024-08-27 08:35:49 +0800

    importing components on profile-edit.page.ts

--- EZPark/src/app/profile-edit/profile-edit.page.ts
+++ EZPark/src/app/profile-edit/profile-edit.page.ts
@@ -1,20 +1,72 @@
 import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonRow,
+  IonButtons,
+  IonCol,
+  IonSearchbar,
+  IonCard,
+  IonIcon,
+  IonItem,
+  IonCardContent,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardHeader,
+  IonImg,
+  IonLabel,
+  IonList,
+  IonMenuButton,
+  IonButton,
+  IonMenu,
+  IonApp,
+  IonText,
+  IonGrid,
+  IonThumbnail,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-profile-edit',
   templateUrl: './profile-edit.page.html',
   styleUrls: ['./profile-edit.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonGrid,
+    IonText,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonRow,
+    IonButtons,
+    IonCol,
+    IonSearchbar,
+    IonCard,
+    IonIcon,
+    IonItem,
+    IonCardContent,
+    IonCardTitle,
+    IonCardSubtitle,
+    IonCardHeader,
+    IonImg,
+    IonLabel,
+    IonList,
+    IonMenuButton,
+    IonButton,
+    IonMenu,
+    IonApp,
+    IonThumbnail,
+    RouterModule,
+  ],
 })
 export class ProfileEditPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
