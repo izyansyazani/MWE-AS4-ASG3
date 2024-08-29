b2cba420b19d030f18849ab46bd95199d9a0667e
22FTT1518 <22ftt1518@student.pb.edu.bn>
2024-08-29 14:43:28 +0800

    add h1 'ratings & reviews'

--- EZPark/src/app/feedback/feedback.page.ts
+++ EZPark/src/app/feedback/feedback.page.ts
@@ -1,20 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonItem,
+  IonGrid,
+  IonRow,
+  IonCardContent,
+  IonLabel,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-feedback',
   templateUrl: './feedback.page.html',
   styleUrls: ['./feedback.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonLabel,
+    IonCardContent,
+    IonRow,
+    IonGrid,
+    IonItem,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+  ],
 })
 export class FeedbackPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
