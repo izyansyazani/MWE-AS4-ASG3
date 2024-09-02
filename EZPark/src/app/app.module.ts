67670d1504cd7cfba952b8685f1e6b217f9ab318
22FTT1518 <22ftt1518@student.pb.edu.bn>
2024-09-02 15:02:17 +0800

    add boxes

--- EZPark/src/app/app.module.ts
+++ EZPark/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { environment } from '../environments/environment';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { provideStorage, getStorage } from '@angular/fire/storage';
-import { AngularFireModule} from '@angular/fire/compat';
+import { AngularFireModule } from '@angular/fire/compat';
 
 @NgModule({
   declarations: [AppComponent],
