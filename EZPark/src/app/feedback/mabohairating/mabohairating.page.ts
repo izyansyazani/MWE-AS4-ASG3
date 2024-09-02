a1a907ac05f8242e9f31e1e6af56a80bd89ed7ea
22FTT1518 <22ftt1518@student.pb.edu.bn>
2024-09-02 12:56:55 +0800

    add backbutton for all 6 pages

--- EZPark/src/app/feedback/mabohairating/mabohairating.page.ts
+++ EZPark/src/app/feedback/mabohairating/mabohairating.page.ts
@@ -1,20 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-mabohairating',
   templateUrl: './mabohairating.page.html',
   styleUrls: ['./mabohairating.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+  ],
 })
 export class MabohairatingPage implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  goToHome() {
+    this.router.navigate(['/home']);
   }
-
 }
--- EZPark/src/app/feedback/timessquarerating/timessquarerating.page.ts
+++ EZPark/src/app/feedback/timessquarerating/timessquarerating.page.ts
@@ -1,20 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-timessquarerating',
   templateUrl: './timessquarerating.page.html',
   styleUrls: ['./timessquarerating.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+  ],
 })
 export class TimessquareratingPage implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  goToHome() {
+    this.router.navigate(['/home']);
   }
-
 }
