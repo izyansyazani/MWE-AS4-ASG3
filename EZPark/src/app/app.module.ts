65b2a9a29f3f39bae20fb8c30a2e365e8fbc5fe2
hasyir <22FTT1502@student.pb.edu.bn>
2024-09-02 15:03:14 +0800

    chore: Add AngularFirestoreModule to app module

--- EZPark/src/app/app.module.ts
+++ EZPark/src/app/app.module.ts
@@ -11,6 +11,8 @@ import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { provideStorage, getStorage } from '@angular/fire/storage';
 import { AngularFireModule} from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
 
 @NgModule({
   declarations: [AppComponent],
