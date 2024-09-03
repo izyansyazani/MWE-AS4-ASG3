fcc5f3ded65269d5a0db7a54febce2f35619e797
22FTT1519 <22ftt1519@student.pb.edu.bn>
2024-09-03 14:51:55 +0800

    receipts for booking

--- EZPark/src/app/payment/payment.page.ts
+++ EZPark/src/app/payment/payment.page.ts
@@ -1,20 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.page.html',
   styleUrls: ['./payment.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+  ],
 })
 export class PaymentPage implements OnInit {
+  bookingDetails: any;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.route.queryParams.subscribe((params) => {
+      if (params['bookingDetails']) {
+        this.bookingDetails = JSON.parse(params['bookingDetails']);
+      }
+    });
   }
-
 }
