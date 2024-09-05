87880ea2d609d3d487507a8bbd06fc5769b9081a
ivy <ivy@gmail.com>
2024-09-05 14:54:48 +0800

    update app-routing.module.ts

--- EZPark/src/app/app-routing.module.ts
+++ EZPark/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileEditPage } from './profile-edit/profile-edit.page';
 
 export const routes: Routes = [
   {
