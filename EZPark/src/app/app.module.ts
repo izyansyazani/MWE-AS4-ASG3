8aec946d53cf95ccb55b42e79827480a40665e8f
22FTT1518 <22ftt1518@student.pb.edu.bn>
2024-09-11 00:05:49 +0800

    Remove the redundant imports of AngularFireModule and AngularFirestoreModule

--- EZPark/src/app/app.module.ts
+++ EZPark/src/app/app.module.ts
@@ -11,8 +11,6 @@ import { environment } from '../environments/environment';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { provideStorage, getStorage } from '@angular/fire/storage';
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
 @NgModule({
   declarations: [AppComponent, ReservationPage, PaymentPage],
