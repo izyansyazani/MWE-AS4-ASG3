e55237fee055c54f88602a97f10bd1b2e3b38a41
22FTT1518 <22ftt1518@student.pb.edu.bn>
2024-09-11 00:35:12 +0800

    add Firebase data fetching for user image, name, recent, and favorite parking

--- EZPark/src/app/home/home.page.ts
+++ EZPark/src/app/home/home.page.ts
@@ -28,6 +28,21 @@ import {
   IonThumbnail,
 } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
+import {
+  Firestore,
+  collectionData,
+  collection,
+  doc,
+  docData,
+} from '@angular/fire/firestore';
+import { Storage, ref, getDownloadURL } from '@angular/fire/storage';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+interface ParkingLocation {
+  title: string;
+  imageUrl: string;
+}
 
 @Component({
   selector: 'app-home',
@@ -69,25 +84,44 @@ export class HomePage implements OnInit {
   recentParkingTitle = 'Recent Parking';
   favouriteParkingTitle = 'Favourite Parking';
 
-  parkingLocations = [
-    {
-      title: 'The Mall, Gadong',
-      imageUrl: '../assets/TheMall.jpg',
-    },
-  ];
+  userImage$: Observable<string>;
+  userName$: Observable<string>;
+  recentParking$: Observable<ParkingLocation[]>;
+  favouriteParking$: Observable<ParkingLocation[]>;
 
-  favouriteParking = [
-    {
-      title: 'The Mall, Gadong',
-      imageUrl: '../assets/TheMall.jpg',
-    },
-    {
-      title: 'Shopping Mall Badong',
-      imageUrl: '../assets/ShoppingMall.jpg',
-    },
-  ];
+  constructor(private firestore: Firestore, private storage: Storage) {}
 
-  constructor() {}
+  ngOnInit() {
+    this.userImage$ = this.getUserImage();
+    this.userName$ = this.getUserName();
+    this.recentParking$ = this.getRecentParking();
+    this.favouriteParking$ = this.getFavouriteParking();
+  }
 
-  ngOnInit() {}
+  getUserImage(): Observable<string> {
+    const imageRef = ref(this.storage, 'path/to/user/image.jpg');
+    return getDownloadURL(imageRef);
+  }
+
+  getUserName(): Observable<string> {
+    const userDoc = doc(this.firestore, 'users/userId');
+    return docData(userDoc).pipe(map((data) => data.name));
+  }
+
+  getRecentParking(): Observable<ParkingLocation[]> {
+    const recentParkingCollection = collection(this.firestore, 'recentParking');
+    return collectionData(recentParkingCollection) as Observable<
+      ParkingLocation[]
+    >;
+  }
+
+  getFavouriteParking(): Observable<ParkingLocation[]> {
+    const favouriteParkingCollection = collection(
+      this.firestore,
+      'favouriteParking'
+    );
+    return collectionData(favouriteParkingCollection) as Observable<
+      ParkingLocation[]
+    >;
+  }
 }
