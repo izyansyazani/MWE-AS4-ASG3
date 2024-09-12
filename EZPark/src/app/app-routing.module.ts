ed50a44214c33aff23a167a5517c8dfe653d72c0
22FTT1518 <22ftt1518@student.pb.edu.bn>
2024-09-13 00:19:27 +0800

    fix: Update favorite functionality in parkingspots.page
    - Updated parkingspots.page.html for favorite buttons.
    - Modified parkingspots.page.ts to handle favorite logic.
    - Ensured change detection for UI updates.
    - Note: Favorite button text still not updating correctly.
    - Ignore the mistakenly added new page.

--- EZPark/src/app/app-routing.module.ts
+++ EZPark/src/app/app-routing.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { ProfileEditPage } from './profile-edit/profile-edit.page';
 
-export const routes: Routes = [
+const routes: Routes = [
   {
     path: 'home',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
@@ -19,12 +18,49 @@ export const routes: Routes = [
   },
   {
     path: 'signup',
-    loadComponent: () =>
-      import('./signup/signup.page').then((m) => m.SignupPage),
+    loadChildren: () =>
+      import('./pages/signup/signup.module').then((m) => m.SignupPageModule),
   },
   {
     path: 'login',
-    loadComponent: () => import('./login/login.page').then((m) => m.LoginPage),
+    loadChildren: () =>
+      import('./pages/login/login.module').then((m) => m.LoginPageModule),
+  },
+  {
+    path: 'forgot-password',
+    loadChildren: () =>
+      import('./pages/forgot-password/forgot-password.module').then(
+        (m) => m.ForgotPasswordPageModule
+      ),
+  },
+  {
+    path: 'verify',
+    loadChildren: () =>
+      import('./pages/verify/verify.module').then((m) => m.VerifyPageModule),
+  },
+  {
+    path: 'landing',
+    loadChildren: () =>
+      import('./pages/landing/landing.module').then((m) => m.LandingPageModule),
+  },
+  {
+    path: 'confirmation',
+    loadChildren: () =>
+      import('./pages/confirmation/confirmation.module').then(
+        (m) => m.ConfirmationPageModule
+      ),
+  },
+  {
+    path: 'journals',
+    loadChildren: () =>
+      import('./pages/journals/journals.module').then(
+        (m) => m.JournalsPageModule
+      ),
+  },
+  {
+    path: 'journal',
+    loadChildren: () =>
+      import('./pages/journal/journal.module').then((m) => m.JournalPageModule),
   },
   {
     path: 'parkingspots',
@@ -38,11 +74,6 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./history/history.page').then((m) => m.HistoryPage),
   },
-  // {
-  //   path: 'rate-hours',
-  //   loadComponent: () =>
-  //     import('./rate-hours/rate-hours.page').then((m) => m.RateHoursPage),
-  // },
   {
     path: 'userprofile',
     loadComponent: () =>
@@ -58,44 +89,11 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./profile-edit/profile-edit.page').then((m) => m.ProfileEditPage),
   },
-
-{
-  path: 'login',
-  loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
-},
-{
-  path: 'forgot-password',
-  loadChildren: () => import('./pages/forgot-password/forgot-password.module').then( m => m.ForgotPasswordPageModule)
-},
-{
-  path: 'signup',
-  loadChildren: () => import('./pages/signup/signup.module').then( m => m.SignupPageModule)
-},
-{
-  path: 'verify',
-  loadChildren: () => import('./pages/verify/verify.module').then( m => m.VerifyPageModule)
-},
-{
-  path: 'landing',
-  loadChildren: () => import('./pages/landing/landing.module').then( m => m.LandingPageModule)
-},
-{
-  path: 'confirmation',
-  loadChildren: () => import('./pages/confirmation/confirmation.module').then( m => m.ConfirmationPageModule)
-},
-{
-  path: 'journals',
-  loadChildren: () => import('./pages/journals/journals.module').then( m => m.JournalsPageModule)
-},
-{
-  path: 'journal',
-  loadChildren: () => import('./pages/journal/journal.module').then( m => m.JournalPageModule)
-},
-
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })    
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
   exports: [RouterModule],
 })
--- EZPark/src/app/app.module.ts
+++ EZPark/src/app/app.module.ts
@@ -1,32 +1,77 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-// import { FormsModule } from '@angular/forms';
+<ion-content>
+  <img
+    (click)="goToHome()"
+    id="back"
+    src="../assets/backbutton.png"
+    class="back-button"
+  />
+  <h1>Find Parking:</h1>
 
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-import { provideStorage, getStorage } from '@angular/fire/storage';
+  <div class="cards-container">
+    <ion-card (click)="goToMall()">
+      <img src="../assets/Themall.jpg" />
+      <ion-card-content>The Mall, Gadong</ion-card-content>
+      <ion-button
+        class="favorite-btn"
+        (click)="toggleFavorite($event, 'The Mall, Gadong')"
+        >{{ isFavorite('The Mall, Gadong') ? 'Unfavorite' : 'Favorite'
+        }}</ion-button
+      >
+    </ion-card>
 
-@NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(),
-    AppRoutingModule,
-    FormsModule,
-    provideFirebaseApp(() => {
-      const app = initializeApp(environment.firebase);
-      return app;
-    }),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()),
-    provideStorage(() => getStorage()),
-  ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
-})
-export class AppModule {}
+    <ion-card>
+      <img src="../assets/Timessquare.jpg" />
+      <ion-card-content>Times Square</ion-card-content>
+      <ion-button
+        class="favorite-btn"
+        (click)="toggleFavorite($event, 'Times Square')"
+        >{{ isFavorite('Times Square') ? 'Unfavorite' : 'Favorite'
+        }}</ion-button
+      >
+    </ion-card>
+
+    <ion-card (click)="goToAirportMall()">
+      <img src="../assets/Theairportmall.jpg" />
+      <ion-card-content>Airport Mall</ion-card-content>
+      <ion-button
+        class="favorite-btn"
+        (click)="toggleFavorite($event, 'Airport Mall')"
+        >{{ isFavorite('Airport Mall') ? 'Unfavorite' : 'Favorite'
+        }}</ion-button
+      >
+    </ion-card>
+
+    <ion-card>
+      <img src="../assets/Yayasanmall.jpg" />
+      <ion-card-content>Yayasan Complex</ion-card-content>
+      <ion-button
+        class="favorite-btn"
+        (click)="toggleFavorite($event, 'Yayasan Complex')"
+        >{{ isFavorite('Yayasan Complex') ? 'Unfavorite' : 'Favorite'
+        }}</ion-button
+      >
+    </ion-card>
+
+    <ion-card>
+      <img src="../assets/mabohai.jpg" />
+      <ion-card-content>Mabohai Shopping Complex</ion-card-content>
+      <ion-button
+        class="favorite-btn"
+        (click)="toggleFavorite($event, 'Mabohai Shopping Complex')"
+        >{{ isFavorite('Mabohai Shopping Complex') ? 'Unfavorite' : 'Favorite'
+        }}</ion-button
+      >
+    </ion-card>
+
+    <ion-card (click)="goToAmanHills()">
+      <img src="../assets/amanhills.jpg" />
+      <ion-card-content>Aman Hills Brunei</ion-card-content>
+      <ion-button
+        class="favorite-btn"
+        (click)="toggleFavorite($event, 'Aman Hills Brunei')"
+        >{{ isFavorite('Aman Hills Brunei') ? 'Unfavorite' : 'Favorite'
+        }}</ion-button
+      >
+    </ion-card>
+  </div>
+</ion-content>
