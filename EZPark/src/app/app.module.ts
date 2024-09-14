45c183747bcc0afdb3f50ce40e715653f9c6c492
hasyir <22FTT1502@student.pb.edu.bn>
2024-09-14 12:17:25 +0800

    Remove commented-out HTML code from app.module.ts

--- EZPark/src/app/app.module.ts
+++ EZPark/src/app/app.module.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -16,6 +15,7 @@ import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFireModule } from '@angular/fire/app';
 import { SignupPageRoutingModule } from './pages/signup/signup-routing.module';
 import { SignupPage } from './pages/signup/signup.page';
+
 
 @NgModule({
   declarations: [AppComponent, SignupPage],
@@ -35,82 +35,80 @@ import { SignupPage } from './pages/signup/signup.page';
   bootstrap: [AppComponent]
 })
 export class AppModule {}
-=======
-<ion-content>
-  <img
-    (click)="goToHome()"
-    id="back"
-    src="../assets/backbutton.png"
-    class="back-button"
-  />
-  <h1>Find Parking:</h1>
+// <ion-content>
+//   <img
+//     (click)="goToHome()"
+//     id="back"
+//     src="../assets/backbutton.png"
+//     class="back-button"
+//   />
+//   <h1>Find Parking:</h1>
 
-  <div class="cards-container">
-    <ion-card (click)="goToMall()">
-      <img src="../assets/Themall.jpg" />
-      <ion-card-content>The Mall, Gadong</ion-card-content>
-      <ion-button
-        class="favorite-btn"
-        (click)="toggleFavorite($event, 'The Mall, Gadong')"
-        >{{ isFavorite('The Mall, Gadong') ? 'Unfavorite' : 'Favorite'
-        }}</ion-button
-      >
-    </ion-card>
+//   <div class="cards-container">
+//     <ion-card (click)="goToMall()">
+//       <img src="../assets/Themall.jpg" />
+//       <ion-card-content>The Mall, Gadong</ion-card-content>
+//       <ion-button
+//         class="favorite-btn"
+//         (click)="toggleFavorite($event, 'The Mall, Gadong')"
+//         >{{ isFavorite('The Mall, Gadong') ? 'Unfavorite' : 'Favorite'
+//         }}</ion-button
+//       >
+//     </ion-card>
 
-    <ion-card>
-      <img src="../assets/Timessquare.jpg" />
-      <ion-card-content>Times Square</ion-card-content>
-      <ion-button
-        class="favorite-btn"
-        (click)="toggleFavorite($event, 'Times Square')"
-        >{{ isFavorite('Times Square') ? 'Unfavorite' : 'Favorite'
-        }}</ion-button
-      >
-    </ion-card>
+//     <ion-card>
+//       <img src="../assets/Timessquare.jpg" />
+//       <ion-card-content>Times Square</ion-card-content>
+//       <ion-button
+//         class="favorite-btn"
+//         (click)="toggleFavorite($event, 'Times Square')"
+//         >{{ isFavorite('Times Square') ? 'Unfavorite' : 'Favorite'
+//         }}</ion-button
+//       >
+//     </ion-card>
 
-    <ion-card (click)="goToAirportMall()">
-      <img src="../assets/Theairportmall.jpg" />
-      <ion-card-content>Airport Mall</ion-card-content>
-      <ion-button
-        class="favorite-btn"
-        (click)="toggleFavorite($event, 'Airport Mall')"
-        >{{ isFavorite('Airport Mall') ? 'Unfavorite' : 'Favorite'
-        }}</ion-button
-      >
-    </ion-card>
+//     <ion-card (click)="goToAirportMall()">
+//       <img src="../assets/Theairportmall.jpg" />
+//       <ion-card-content>Airport Mall</ion-card-content>
+//       <ion-button
+//         class="favorite-btn"
+//         (click)="toggleFavorite($event, 'Airport Mall')"
+//         >{{ isFavorite('Airport Mall') ? 'Unfavorite' : 'Favorite'
+//         }}</ion-button
+//       >
+//     </ion-card>
 
-    <ion-card>
-      <img src="../assets/Yayasanmall.jpg" />
-      <ion-card-content>Yayasan Complex</ion-card-content>
-      <ion-button
-        class="favorite-btn"
-        (click)="toggleFavorite($event, 'Yayasan Complex')"
-        >{{ isFavorite('Yayasan Complex') ? 'Unfavorite' : 'Favorite'
-        }}</ion-button
-      >
-    </ion-card>
+//     <ion-card>
+//       <img src="../assets/Yayasanmall.jpg" />
+//       <ion-card-content>Yayasan Complex</ion-card-content>
+//       <ion-button
+//         class="favorite-btn"
+//         (click)="toggleFavorite($event, 'Yayasan Complex')"
+//         >{{ isFavorite('Yayasan Complex') ? 'Unfavorite' : 'Favorite'
+//         }}</ion-button
+//       >
+//     </ion-card>
 
-    <ion-card>
-      <img src="../assets/mabohai.jpg" />
-      <ion-card-content>Mabohai Shopping Complex</ion-card-content>
-      <ion-button
-        class="favorite-btn"
-        (click)="toggleFavorite($event, 'Mabohai Shopping Complex')"
-        >{{ isFavorite('Mabohai Shopping Complex') ? 'Unfavorite' : 'Favorite'
-        }}</ion-button
-      >
-    </ion-card>
+//     <ion-card>
+//       <img src="../assets/mabohai.jpg" />
+//       <ion-card-content>Mabohai Shopping Complex</ion-card-content>
+//       <ion-button
+//         class="favorite-btn"
+//         (click)="toggleFavorite($event, 'Mabohai Shopping Complex')"
+//         >{{ isFavorite('Mabohai Shopping Complex') ? 'Unfavorite' : 'Favorite'
+//         }}</ion-button
+//       >
+//     </ion-card>
 
-    <ion-card (click)="goToAmanHills()">
-      <img src="../assets/amanhills.jpg" />
-      <ion-card-content>Aman Hills Brunei</ion-card-content>
-      <ion-button
-        class="favorite-btn"
-        (click)="toggleFavorite($event, 'Aman Hills Brunei')"
-        >{{ isFavorite('Aman Hills Brunei') ? 'Unfavorite' : 'Favorite'
-        }}</ion-button
-      >
-    </ion-card>
-  </div>
-</ion-content>
->>>>>>> ed50a44214c33aff23a167a5517c8dfe653d72c0
+//     <ion-card (click)="goToAmanHills()">
+//       <img src="../assets/amanhills.jpg" />
+//       <ion-card-content>Aman Hills Brunei</ion-card-content>
+//       <ion-button
+//         class="favorite-btn"
+//         (click)="toggleFavorite($event, 'Aman Hills Brunei')"
+//         >{{ isFavorite('Aman Hills Brunei') ? 'Unfavorite' : 'Favorite'
+//         }}</ion-button
+//       >
+//     </ion-card>
+//   </div>
+// </ion-content>
