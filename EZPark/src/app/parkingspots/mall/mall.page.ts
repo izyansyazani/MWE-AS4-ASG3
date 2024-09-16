1306f96007e9d9505036b9c6073032736e7b48f6
22FTT1519 <22ftt1519@student.pb.edu.bn>
2024-09-16 22:29:43 +0800

    test booking spot

--- EZPark/src/app/parkingspots/mall/mall.page.ts
+++ EZPark/src/app/parkingspots/mall/mall.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 import {
   IonContent,
   IonHeader,
@@ -54,6 +55,33 @@ import {
   ],
 })
 export class MallPage implements OnInit {
+  bookedSpots: { [key: string]: boolean } = {
+    A1: false,
+    A2: false,
+    A3: false,
+    A4: false,
+    A5: false,
+    A6: false,
+    B1: false,
+    B2: false,
+    B3: false,
+    B4: false,
+    B5: false,
+    B6: false,
+    C1: false,
+    C2: false,
+    C3: false,
+    C4: false,
+    C5: false,
+    C6: false,
+    D1: false,
+    D2: false,
+    D3: false,
+    D4: false,
+    D5: false,
+    D6: false,
+  };
+
   constructor(private router: Router) {}
 
   ngOnInit() {}
