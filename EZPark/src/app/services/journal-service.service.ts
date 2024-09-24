da5a1824707814db54eb14261415169bec329034
22FTT1519 <22ftt1519@student.pb.edu.bn>
2024-09-24 12:08:41 +0800

    update imports for services after moving services into services file

--- EZPark/src/app/services/journal-service.service.ts
+++ EZPark/src/app/services/journal-service.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { Firestore, addDoc, collection, collectionData, doc, docData, query, updateDoc, where } from '@angular/fire/firestore';
+import {
+  Firestore,
+  addDoc,
+  collection,
+  collectionData,
+  doc,
+  docData,
+  query,
+  updateDoc,
+  where,
+} from '@angular/fire/firestore';
 
-import { AuthServiceService } from '../auth-service.service';
+import { AuthServiceService } from './auth-service.service';
 
 import firebase from 'firebase/compat/app';
 import { Observable } from 'rxjs';
@@ -10,13 +20,12 @@ import { deleteDoc } from 'firebase/firestore';
 // import 'firebase/compat/firestore';
 // import firebase from "firebase/compat/app";
 
-
-export class Journal{
-  id? : string;
-  userId:string;
+export class Journal {
+  id?: string;
+  userId: string;
   title: string;
-  content:string;
-  createdAt:any
+  content: string;
+  createdAt: any;
 
   constructor(userId: string, title: string, content: string, createdAt: any) {
     this.userId = userId;
@@ -24,71 +33,71 @@ export class Journal{
     this.content = content;
     this.createdAt = createdAt;
   }
-  
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class JournalServiceService {
+  private journalCollection: any;
+  private userId: any;
 
-  private journalCollection:any
-  private userId:any 
-
-  constructor(private firestore:Firestore,private authService:AuthServiceService) {
-    this.authService.getProfile().then(user => {
-
-      this.userId = user?.uid;
-      console.log(user?.uid);
-      // this.journalCollection = co;;
-
-    }).catch(error => {
-      console.error('Error getting user profile:', error);
-    });
+  constructor(
+    private firestore: Firestore,
+    private authService: AuthServiceService
+  ) {
+    this.authService
+      .getProfile()
+      .then((user) => {
+        this.userId = user?.uid;
+        console.log(user?.uid);
+        // this.journalCollection = co;;
+      })
+      .catch((error) => {
+        console.error('Error getting user profile:', error);
+      });
   }
 
-
-   addJournal(journal:Journal){
+  addJournal(journal: Journal) {
     journal.userId = this.userId;
     console.log(journal);
-    
+
     const jouralRef = collection(this.firestore, 'notes');
-    
-    return addDoc(jouralRef,journal)
+
+    return addDoc(jouralRef, journal);
   }
 
-  getJournals(userId:any): Observable<Journal[]> {
-    
-    const jouralRef = collection(this.firestore, 'notes')
-    
-    const  refq =  query(jouralRef,where('userId','==',userId))
-    return collectionData(refq,{ idField: 'id'}) as Observable<Journal[]>
+  getJournals(userId: any): Observable<Journal[]> {
+    const jouralRef = collection(this.firestore, 'notes');
+
+    const refq = query(jouralRef, where('userId', '==', userId));
+    return collectionData(refq, { idField: 'id' }) as Observable<Journal[]>;
   }
 
-   getUser(){
-     this.authService.getProfile().then(user =>{
-      this.userId = user?.uid
+  getUser() {
+    this.authService.getProfile().then((user) => {
+      this.userId = user?.uid;
       console.log(this.userId);
-    })
+    });
 
-   return this.userId
+    return this.userId;
   }
 
-
-  getJournalById(id:any) : Observable<Journal>{
-      const journalRef = doc(this.firestore,`notes/${id}`)
-      return docData(journalRef,{ idField: 'id'}) as Observable<Journal>
-  }
-  
-  removeJournal(id:any){
-    const journalRef = doc(this.firestore, `notes/${id}`)
-    return deleteDoc(journalRef)
+  getJournalById(id: any): Observable<Journal> {
+    const journalRef = doc(this.firestore, `notes/${id}`);
+    return docData(journalRef, { idField: 'id' }) as Observable<Journal>;
   }
 
-  updateJournal(journal:Journal){
-    const journalRef = doc(this.firestore,`notes/${journal.id}`)
-    return updateDoc(journalRef,{title:journal.title,content:journal.content})
+  removeJournal(id: any) {
+    const journalRef = doc(this.firestore, `notes/${id}`);
+    return deleteDoc(journalRef);
   }
 
-
+  updateJournal(journal: Journal) {
+    const journalRef = doc(this.firestore, `notes/${journal.id}`);
+    return updateDoc(journalRef, {
+      title: journal.title,
+      content: journal.content,
+    });
+  }
 }
