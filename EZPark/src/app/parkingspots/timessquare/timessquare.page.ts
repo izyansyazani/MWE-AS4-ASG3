19c6ebfaac4f29940dd5ae1b977d16ddd89f6156
ivy <ivy@gmail.com>
2024-09-29 21:25:07 +0800

    filling the times square pages

--- EZPark/src/app/parkingspots/timessquare/timessquare.page.ts
+++ EZPark/src/app/parkingspots/timessquare/timessquare.page.ts
@@ -1,20 +1,69 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonRow,
+  IonButton,
+  IonCol,
+  IonSearchbar,
+  IonCard,
+  IonIcon,
+  IonItem,
+  IonCardContent,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardHeader,
+  IonImg,
+  IonLabel,
+  IonGrid,
+  IonButtons,
+  IonBackButton,
+} from '@ionic/angular/standalone';
 
 @Component({
-  selector: 'app-timessquare',
+  selector: 'app-times-square', // Changed the selector
   templateUrl: './timessquare.page.html',
   styleUrls: ['./timessquare.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonRow,
+    IonButton,
+    IonCol,
+    IonSearchbar,
+    IonCard,
+    IonIcon,
+    IonItem,
+    IonCardContent,
+    IonCardTitle,
+    IonCardSubtitle,
+    IonCardHeader,
+    IonImg,
+    IonLabel,
+    IonGrid,
+    IonButtons,
+    IonBackButton,
+  ],
 })
-export class TimessquarePage implements OnInit {
+export class TimesSquarePage implements OnInit {
+  // Changed to TimesSquarePage
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  goToParking() {
+    this.router.navigate(['/parkingspots']);
   }
 
+  goToBook() {
+    this.router.navigate(['/reservation']);
+  }
 }
