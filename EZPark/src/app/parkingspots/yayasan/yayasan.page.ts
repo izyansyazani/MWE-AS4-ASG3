5ef2ae94c5b0cc3aa14f64065e53ffea5157ff51
ivy <ivy@gmail.com>
2024-09-29 21:30:52 +0800

    Refactor Yayasan page layout and styling

--- EZPark/src/app/parkingspots/yayasan/yayasan.page.ts
+++ EZPark/src/app/parkingspots/yayasan/yayasan.page.ts
@@ -1,20 +1,69 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonRow,
+  IonButton,
+  IonCol,
+  IonSearchbar,
+  IonCard,
+  IonIcon,
+  IonItem,
+  IonCardContent,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardHeader,
+  IonImg,
+  IonLabel,
+  IonGrid,
+  IonButtons,
+  IonBackButton,
+} from '@ionic/angular/standalone';
 
 @Component({
-  selector: 'app-yayasan',
+  selector: 'app-yayasan', // Changed the selector
   templateUrl: './yayasan.page.html',
   styleUrls: ['./yayasan.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonRow,
+    IonButton,
+    IonCol,
+    IonSearchbar,
+    IonCard,
+    IonIcon,
+    IonItem,
+    IonCardContent,
+    IonCardTitle,
+    IonCardSubtitle,
+    IonCardHeader,
+    IonImg,
+    IonLabel,
+    IonGrid,
+    IonButtons,
+    IonBackButton,
+  ],
 })
 export class YayasanPage implements OnInit {
+  // Changed to TimesSquarePage
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  goToParking() {
+    this.router.navigate(['/parkingspots']);
   }
 
+  goToBook() {
+    this.router.navigate(['/reservation']);
+  }
 }
