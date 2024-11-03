e10b3377aa72107b75eb55bc30c4da36189007e7
22FTT1519 <22ftt1519@student.pb.edu.bn>
2024-11-03 23:58:39 +0800

    added realtimedatabase url

--- EZPark/src/environments/environment.ts
+++ EZPark/src/environments/environment.ts
@@ -1,12 +1,14 @@
 export const environment = {
   production: false,
-  firebaseConfig : {
-  apiKey: "AIzaSyBPkqCi7EM1WWmJ755fmCcjvH15-RYCuAc",
-  authDomain: "ezpark-7ec4d.firebaseapp.com",
-  projectId: "ezpark-7ec4d",
-  storageBucket: "ezpark-7ec4d.appspot.com",
-  messagingSenderId: "495351849846",
-  appId: "1:495351849846:web:1fe95033e09a44a63ed057",
-  measurementId: "G-VLQB2P7SE8"
-  }
+  firebaseConfig: {
+    apiKey: 'AIzaSyBPkqCi7EM1WWmJ755fmCcjvH15-RYCuAc',
+    authDomain: 'ezpark-7ec4d.firebaseapp.com',
+    projectId: 'ezpark-7ec4d',
+    storageBucket: 'ezpark-7ec4d.appspot.com',
+    messagingSenderId: '495351849846',
+    appId: '1:495351849846:web:1fe95033e09a44a63ed057',
+    measurementId: 'G-VLQB2P7SE8',
+    databaseURL:
+      'https://ezpark-7ec4d-default-rtdb.asia-southeast1.firebasedatabase.app/',
+  },
 };
